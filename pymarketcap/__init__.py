553e6db81c4341e13f51733085f62349afbee2f4
mondeja <mondejar1994@gmail.com>
2017-04-01 13:03:50 +0200

    Add files via upload

---
+++ pymarketcap/__init__.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+__title__   = 'pymarketcap'
+__version__ = '3.0.1'
+__author__ = 'Álvaro Mondéjar <mondejar1994@gmail.com>'
+__repo__    = 'https://github.com/mrsmn/pymarketcap'
+__license__ = 'Apache v2.0 License'
+
+from .core import pymarketcap
+
