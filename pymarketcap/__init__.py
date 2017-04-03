085a36c0db9313b55255f01d91581870f16e8a54
mondeja <mondejar1994@gmail.com>
2017-04-03 12:42:48 +0200

    Update __init__.py

--- pymarketcap/__init__.py
+++ pymarketcap/__init__.py
@@ -4,8 +4,8 @@
 __title__   = 'pymarketcap'
 __version__ = '3.0.1'
 __author__ = 'Álvaro Mondéjar <mondejar1994@gmail.com>'
-__repo__    = 'https://github.com/mrsmn/pymarketcap'
+__repo__    = 'https://github.com/mondeja/pymarketcap'
 __license__ = 'Apache v2.0 License'
 
-from .core import pymarketcap
+from .core import Pymarketcap
 
