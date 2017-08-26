f7e2127dbd57a92dfa939359b049d6a010fb1d8a
mondeja <mondejar1994@gmail.com>
2017-08-26 18:32:54 +0200

    New version 3.2.0

--- pymarketcap/__init__.py
+++ pymarketcap/__init__.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 
 __title__   = 'pymarketcap'
-__version__ = '3.0.1'
+__version__ = '3.2.0'
 __author__ = 'Álvaro Mondéjar <mondejar1994@gmail.com>'
 __repo__    = 'https://github.com/mondeja/pymarketcap'
 __license__ = 'Apache v2.0 License'
