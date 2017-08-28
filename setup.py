c3552ccad63c2e3ea54102466dd1d7e981c1cca2
mondeja <mondejar1994@gmail.com>
2017-08-28 18:33:06 +0200

    Add files via upload

--- setup.py
+++ setup.py
@@ -5,14 +5,14 @@ from distutils.core import setup
 
 setup(
     name = 'pymarketcap',
-    version = '3.0.1',
+    version = '3.2.0',
     url = 'https://github.com/mondeja/pymarketcap',
     download_url = 'https://github.com/mondeja/pymarketcap/archive/master.zip',
     author = 'Álvaro Mondéjar <mondejar1994@gmail.com>',
     author_email = 'mondejar1994@gmail.com',
     license = 'Apache v2.0 License',
     packages = ['pymarketcap'],
-    description = 'Python2 API for coinmarketcap.com.',
+    description = 'Python3 API and web scraper for coinmarketcap.com.',
     long_description = open('README.md','r').read(),
-    keywords = ['Scrypt', 'SHA256d', 'cryptocurrency', 'API', 'wrapper', 'parser', 'json', 'LTC', 'Litecoin', 'BTC', 'Bitcoin'],
+    keywords = ['coinmarketcap', 'cryptocurrency', 'API', 'wrapper', 'scraper', 'json', 'BTC', 'Bitcoin'],
 )
