34a1f7dd09a9c85011fcd7d6c3e8f9349a8443ba
mondeja <mondejar1994@gmail.com>
2018-01-26 15:23:55 +0100

    PyPi version updated and ignore problems with Travis MacOSX environment

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ with open(os.path.join(BASEDIR, "README.md"), encoding="utf-8") as f:
 
 setup(
     name = 'pymarketcap',
-    version = '3.3.150',
+    version = '3.3.151',
     url = 'https://github.com/mondeja/pymarketcap',
     download_url = 'https://github.com/mondeja/pymarketcap/archive/master.zip',
     author = 'Álvaro Mondéjar',
