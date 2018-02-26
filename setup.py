bb2ce4d739ddf5a4a6f709aa5c1db4266cdd6d91
mondeja <mondejar1994@gmail.com>
2018-02-26 23:30:46 +0100

    IFrames added to live documentation

--- setup.py
+++ setup.py
@@ -121,7 +121,7 @@ with open(os.path.join(os.path.dirname(__file__), "requirements.txt")) as f:
 
 setup(
     name="pymarketcap",
-    version = "3.9.116",
+    version = "3.9.117",
     url = "https://github.com/mondeja/pymarketcap",
     download_url = "https://github.com/mondeja/pymarketcap/archive/master.zip",
     author = "Álvaro Mondéjar Rubio",
