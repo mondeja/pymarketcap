cee7b0e432414d4c9a7ea61fba2bbe6d47eaf3e5
travis-ci <travis@travis-ci.org>
2018-03-31 01:18:30 +0200

    TravisCI config update

--- doc/conf.py
+++ doc/conf.py
@@ -28,7 +28,7 @@ author = 'Álvaro Mondéjar Rubio'
 
 
 # The full version, including alpha/beta/rc tags
-release = "3.9.157"
+release = "3.9.158"
 # The short X.Y version
 version = ".".join(release.split(".")[:-1])
 
